6ada48b4e07845a7feac4b92942f3e76a0b7d983
joaoapm <joaoapm@gmail.com>
2018-11-20 11:19:16 -0200

    etc

--- jvtst/src/jvtst/JavaRegex.java
+++ jvtst/src/jvtst/JavaRegex.java
@@ -1,10 +1,8 @@
 package jvtst;
 
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 import java.util.Scanner;
 
-class JavaRegex {
+public class JavaRegex {
 
 	public static void main(String[] args) {
 		Scanner in = new Scanner(System.in);
