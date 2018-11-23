2d8865f52742c4617702c54df1601a374190889b
joaoapm <joaoapm@gmail.com>
2018-11-22 22:59:27 -0200

    etc

---
+++ jvtst/src/jvtst/JavaInterface.java
@@ -0,0 +1,52 @@
+package jvtst;
+
+import java.util.*;
+
+interface AdvancedArithmetic {
+	int divisor_sum(int n);
+}
+
+class MyCalculator implements AdvancedArithmetic {
+
+	@Override
+	public int divisor_sum(int n) {
+		int sum = 0;
+
+		int div = n;
+
+		while (n > 0) {
+			
+			if (div % n == 0)
+				sum += n;
+			
+			n--;
+		}
+		// TODO Auto-generated method stub
+		return sum;
+	}
+
+}
+
+class JavaInterface {
+	public static void main(String[] args) {
+		MyCalculator my_calculator = new MyCalculator();
+		System.out.print("I implemented: ");
+		ImplementedInterfaceNames(my_calculator);
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		System.out.print(my_calculator.divisor_sum(n) + "\n");
+		sc.close();
+	}
+
+	/*
+	 * ImplementedInterfaceNames method takes an object and prints the name of the
+	 * interfaces it implemented
+	 */
+	static void ImplementedInterfaceNames(Object o) {
+		Class[] theInterfaces = o.getClass().getInterfaces();
+		for (int i = 0; i < theInterfaces.length; i++) {
+			String interfaceName = theInterfaces[i].getName();
+			System.out.println(interfaceName);
+		}
+	}
+}
