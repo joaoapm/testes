e7b49245387d604b6e33080773b293efdc720089
Joao Prestes Matiuzzi Matiuzzi <joao-matiuzzi@procergs.rs.gov.br>
2019-06-10 10:24:09 -0300

    etc

--- devskiller-code-ATY4-X2Y6-9D-S1F/src/main/java/com/devskiller/tasks/blog/repository/PostRepository.java
+++
@@ -1,9 +0,0 @@
-package com.devskiller.tasks.blog.repository; 
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-import com.devskiller.tasks.blog.model.Post;
-
-public interface PostRepository extends MongoRepository<Post, String> {
-
-}
